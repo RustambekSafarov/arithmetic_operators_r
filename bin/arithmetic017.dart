e33c4ca3aa16b39b46dc9d0570721f1d7b862951
Rustambek <rustambeksafarovv@gmail.com>
2022-07-13 11:36:47 +0500

    Redone

--- bin/arithmetic017.dart
+++ bin/arithmetic017.dart
@@ -1,8 +1,8 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 import 'dart:math';
-double func(a,b){
-    return ((pow(a,2)+b)/(2*pow(b,2))).round();
+num func(a,b){
+    return ((pow(a,2)+b)/(2*pow(b,2)));
 }
 void main() {
     print(func(2,3));
