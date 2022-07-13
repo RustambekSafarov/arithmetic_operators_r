22ee16c19822233dabde8e45179922b13e569134
Rustambek <rustambeksafarovv@gmail.com>
2022-07-13 16:09:32 +0500

    Redone

--- bin/arithmetic022.dart
+++ bin/arithmetic022.dart
@@ -3,9 +3,15 @@
 
 import 'dart:math';
 num func(a,b,c){
+    num y = 0;
     num x = -(2*pow(b,2)-a)/(pow(a,3)-pow(c,2));
-    
-    return x * 100 ~/ 1 /100;
+    if(x<2){
+        y = x * 100 ~/ 1 /100;
+    }
+    else{
+        y = x;
+    }
+    return y;
 }
 void main() {
     print(func(2,4,6));
