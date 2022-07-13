b017594ac83bd7c91be9a8e6df32c90cbe35c365
Rustambek <rustambeksafarovv@gmail.com>
2022-07-13 11:19:34 +0500

    Redone

--- bin/arithmetic020.dart
+++ bin/arithmetic020.dart
@@ -1,4 +1,10 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+import 'dart:math';
+double func(a,b,c){
+    return (a+b)/(2*c);
+}
+void main() {
+    print(func(2,3,4));
+}
