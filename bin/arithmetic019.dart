9219a96ff05753da1c7ef59821abad33d0edf25f
Rustambek <rustambeksafarovv@gmail.com>
2022-07-13 15:41:15 +0500

    Redone

--- bin/arithmetic019.dart
+++ bin/arithmetic019.dart
@@ -2,8 +2,8 @@
 // the task condition is in the Readme file
 import 'dart:math';
 double func(a,b){
-    return pow(a,2)+2*((pow(a,2)+pow(b,2))/4*a);
+    return pow(a,2)+2*((pow(a,2)+pow(b,2))/(4*a));
 }
 void main() {
-    print(func(2,3));
+    print(func(10,4));
 }
