8d6d07d197bfeea7128f8d9017276b0993f32237
Rustambek <rustambeksafarovv@gmail.com>
2022-07-13 11:28:41 +0500

    Redone

--- bin/arithmetic010.dart
+++ bin/arithmetic010.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 // Create function func with arguments
 // the task condition is in the Readme file
-int func(a,b){
+num func(a,b){
     return pow(a,2)+2*a*b+pow(b,2);
 }
 void main() {
