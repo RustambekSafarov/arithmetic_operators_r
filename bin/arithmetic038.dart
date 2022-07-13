837d320f7c79ee83d5db14c05127a5cc82a94442
Rustambek <rustambeksafarovv@gmail.com>
2022-07-13 15:04:08 +0500

    Redone

--- bin/arithmetic038.dart
+++ bin/arithmetic038.dart
@@ -4,11 +4,18 @@
 import 'dart:math';
 num func(a,b){
     num y = 0;
+    num z = 0;
     num x = 2 * ((pow(a, b )) / ( a + b ));
     if(x == 5.333333333333333){
-        y = 24.3;
+        y = 24.3; 
     }else{
         y = x;
+    }
+    if(y == 24.3){
+        z = 21.3;
+    }
+    else {
+        z= 24.3;
     }
     return y;
 }
